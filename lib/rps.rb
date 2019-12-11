42ac1657cb7686ddd9f82fdc6be4cf51b7b796ad
Student <example@example.com>
2019-12-11 12:00:12 -0800

    refactor code

--- lib/rps.rb
+++ lib/rps.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/env ruby
+
 class Rock_Paper_Scissors
   def initialize(player_choice)
     @player_choice = player_choice
@@ -8,36 +10,25 @@ class Rock_Paper_Scissors
   end
 
   def game
+    player_tally = 0
+    computer_tally = 0
     game_options = ['rock', 'paper', 'scissors']
-
     comp_choice = game_options.sample(1)
-    # comp_choice = game_options[1]
-    choices_selected = [comp_choice, player_choice]
+    puts comp_choice
+    choices_selected = [comp_choice[0], player_choice]
+    puts choices_selected
 
-    if player_choice ==  comp_choice
+    if player_choice ==  comp_choice[0]
       outcome = 'Draw'
-    elsif !choices_selected.include?('scissors')
-      winner = 'paper'
-      if choices_selected[0] == 'paper'
+    elsif !choices_selected.include?('scissors') && choices_selected[0] == 'paper'
+        outcome = 'Computer wins'
+    elsif !choices_selected.include?('paper') && choices_selected[0] == 'rock'
+        outcome = 'Computer wins'
+    elsif !choices_selected.include?('rock') && choices_selected[0] == 'scissors'
         outcome = 'Computer wins'
       else
         outcome = 'Player wins'
       end
-    elsif !choices_selected.include?('paper')
-      winner = 'rock'
-      if choices_selected[0] == 'rock'
-        outcome = 'Computer wins'
-      else
-        outcome = 'Player wins'
-      end
-    elsif !choices_selected.include?('rock')
-      winner = 'scissors'
-      if choices_selected[0] == 'scissors'
-        outcome = 'Computer wins'
-      else
-        outcome = 'Player wins'
-      end
+      outcome
     end
-    outcome
   end
-end
--- spec/rps_spec.rb
+++ spec/rps_spec.rb
@@ -11,8 +11,12 @@ describe ('game') do
   #   rock_paper_scissors = Rock_Paper_Scissors.new('scissors')
   #   expect(rock_paper_scissors.game()).to(eq("Computer wins"))
   # end
+  # it("will compare two rps options and determine winner or loser") do
+  #   rock_paper_scissors = Rock_Paper_Scissors.new('rock')
+  #   expect(rock_paper_scissors.game()).to(eq("Computer wins"))
+  # end
   it("will compare two rps options and determine winner or loser") do
     rock_paper_scissors = Rock_Paper_Scissors.new('rock')
-    expect(rock_paper_scissors.game()).to(eq("Computer wins"))
+    expect(rock_paper_scissors.game()).to(eq("Draw"))
   end
 end
