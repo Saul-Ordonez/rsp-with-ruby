518870a953ec8184348c80614f352e846ba823e7
Student <example@example.com>
2019-12-11 09:23:58 -0800

    successfully pass first test

--- lib/rps.rb
+++ lib/rps.rb
@@ -1,23 +1,26 @@
 class Rock_Paper_Scissors
-  def initialize(rock, paper, scissors)
-    @rock = rock
-    @paper = paper
-    @scissors = scissors
+  def initialize(player_choice)
+    @player_choice = player_choice
   end
 
-  def rock
-    @rock
+  def player_choice
+    @player_choice
   end
 
-  def paper
-    @paper
-  end
-
-  def scissors
-    @scissors
-  end
+  # def winner_selector
+  #
+  # end
 
   def game
+    game_options = ['rock', 'paper', 'scissors']
 
+    # player_choice = game_options.sample(1)
+    # comp_choice = game_options.sample(1)
+    comp_choice = game_options[0]
+
+    if player_choice == game_options[0] && comp_choice == game_options[0]
+    outcome = 'Draw'
+    end
+    outcome
   end
 end
--- spec/rps_spec.rb
+++ spec/rps_spec.rb
@@ -4,7 +4,7 @@ require ('rps')
 
 describe ('game') do
   it("will compare two rps options and determine winner") do
-    rock_paper_scissors = Rock_Paper_Scissors.new("rock", "paper")
-    expect(rock_paper_scissors.game())to(eq("paper"))
+    rock_paper_scissors = Rock_Paper_Scissors.new('rock')
+    expect(rock_paper_scissors.game()).to(eq("Draw"))
   end
 end
