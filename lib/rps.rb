cf672efe4075da5ed569d560af6ee5705ed7def9
Student <example@example.com>
2019-12-11 16:24:25 -0800

    add scripting

--- lib/rps.rb
+++ lib/rps.rb
@@ -1,38 +1,51 @@
 #!/usr/bin/env ruby
-
 class Rock_Paper_Scissors
   def initialize(player_choice)
     @player_choice = player_choice
+    @comp_output = ''
   end
 
   def player_choice
     @player_choice
   end
 
+  def comp_output
+    @comp_output
+  end
+
   def game
-    player_tally = 0
-    computer_tally = 0
     game_options = ['rock', 'paper', 'scissors']
     comp_choice = game_options.sample(1)
-    puts comp_choice
-    choices_selected = [comp_choice[0], player_choice]
-    puts choices_selected
+    @comp_output = comp_choice[0]
+    player_index = game_options.index(player_choice)
 
-    if player_choice ==  comp_choice[0]
+    if player_choice == comp_choice[0]
       outcome = 'Draw'
-    elsif !choices_selected.include?('scissors') && choices_selected[0] == 'paper'
-        outcome = 'Computer wins'
-        computer_tally += 1
-    elsif !choices_selected.include?('paper') && choices_selected[0] == 'rock'
-        outcome = 'Computer wins'
-        computer_tally += 1
-    elsif !choices_selected.include?('rock') && choices_selected[0] == 'scissors'
-        outcome = 'Computer wins'
-        computer_tally += 1
-      else
-        outcome = 'Player wins'
-        player_tally += 1
+    elsif comp_choice[0] == game_options[(player_index - 1)]
+      outcome = 'Player wins'
+    else
+      outcome = 'Computer wins'
       end
       outcome
     end
   end
+
+
+# forntend logic
+puts "Let's play Rock, Paper, Scissors!"
+puts "Please choose rock, paper or scissors!"
+
+player_input = gets.chomp
+rps = Rock_Paper_Scissors.new(player_input)
+outcome = rps.game()
+
+puts "You chose #{rps.player_choice}"
+puts "The computer chose #{rps.comp_output}"
+
+if outcome == 'Draw'
+  puts 'Draw'
+elsif outcome == 'Player wins'
+  puts 'You won!!!'
+else
+  puts 'You lose!!!'
+end
