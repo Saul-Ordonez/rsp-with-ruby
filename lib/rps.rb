e3cc9ef5148ff3e4942eb3be10ee9036d523e7c6
Student <example@example.com>
2019-12-11 16:31:31 -0800

    add attr method

--- lib/rps.rb
+++ lib/rps.rb
@@ -1,16 +1,10 @@
 #!/usr/bin/env ruby
 class Rock_Paper_Scissors
+  attr_accessor(:player_choice, :comp_output)
+
   def initialize(player_choice)
     @player_choice = player_choice
     @comp_output = ''
-  end
-
-  def player_choice
-    @player_choice
-  end
-
-  def comp_output
-    @comp_output
   end
 
   def game
@@ -29,23 +23,3 @@ class Rock_Paper_Scissors
       outcome
     end
   end
-
-
-# forntend logic
-puts "Let's play Rock, Paper, Scissors!"
-puts "Please choose rock, paper or scissors!"
-
-player_input = gets.chomp
-rps = Rock_Paper_Scissors.new(player_input)
-outcome = rps.game()
-
-puts "You chose #{rps.player_choice}"
-puts "The computer chose #{rps.comp_output}"
-
-if outcome == 'Draw'
-  puts 'Draw'
-elsif outcome == 'Player wins'
-  puts 'You won!!!'
-else
-  puts 'You lose!!!'
-end
--- script.rb
+++ script.rb
@@ -1,2 +1,21 @@
 #!/usr/bin/env ruby
 require ('./lib/rps.rb')
+
+# forntend logic
+puts "Let's play Rock, Paper, Scissors!"
+puts "Please choose rock, paper or scissors!"
+
+player_input = gets.chomp
+rps = Rock_Paper_Scissors.new(player_input)
+outcome = rps.game()
+
+puts "You chose #{rps.player_choice}"
+puts "The computer chose #{rps.comp_output}"
+
+if outcome == 'Draw'
+  puts 'Draw'
+elsif outcome == 'Player wins'
+  puts 'You won!!!'
+else
+  puts 'You lose!!!'
+end
