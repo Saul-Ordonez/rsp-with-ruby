1e9195d3ef666b02a4909e3e37d789a290892e99
Student <example@example.com>
2019-12-11 16:33:48 -0800

    get rid of a few lines

--- lib/rps.rb
+++ lib/rps.rb
@@ -1,18 +1,14 @@
-#!/usr/bin/env ruby
 class Rock_Paper_Scissors
   attr_accessor(:player_choice, :comp_output)
-
   def initialize(player_choice)
     @player_choice = player_choice
     @comp_output = ''
   end
-
   def game
     game_options = ['rock', 'paper', 'scissors']
     comp_choice = game_options.sample(1)
     @comp_output = comp_choice[0]
     player_index = game_options.index(player_choice)
-
     if player_choice == comp_choice[0]
       outcome = 'Draw'
     elsif comp_choice[0] == game_options[(player_index - 1)]
